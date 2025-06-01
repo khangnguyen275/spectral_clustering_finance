922cdaa7f065897d3fbbb8b3ad59c1313929a17f
khangnguyen275 <khang.nguyenhoang.vn@gmail.com>
2025-05-31 20:27:51 -0700

    added the clusterize function

--- utils/cluster.py
+++ utils/cluster.py
@@ -6,6 +6,8 @@ from scipy.sparse import csc_matrix
 from scipy.linalg import eigh
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+from returns import get_market_residual_returns
 
 def compute_correlation_matrix(residual_returns_matrix, w = 5):
     """
@@ -17,6 +19,7 @@ def compute_correlation_matrix(residual_returns_matrix, w = 5):
 
     Returns:
         np.ndarray: An N x N correlation matrix.
+
     """
     # Take the day that we're on, subtract by w days to get the start date,
     # and then take the period in the next w days
@@ -62,3 +65,61 @@ def plot_correlation_matrix(correlation_matrix, title='Correlation Matrix Heatma
     plt.ylabel('Stocks')
     plt.show()
 
+def clusterize(cl_med: str, num_med: str, R_cov: pd.DataFrame, market_cov, clustering_window=100, default_cluster_num=15):
+  R = R_cov.copy()
+  market = market_cov.copy()
+
+  # compute the correlation matrix used for clusterization
+  residual_returns_matrix = get_market_residual_returns(R, market)
+  residual_returns_matrix = residual_returns_matrix.astype(float).T
+  corr = compute_correlation_matrix(residual_returns_matrix)
+
+  # choose which clusterization method to use
+  if cl_med == 'SPONGE':
+
+    # determine the number of clusters for the SPONGE algorithm
+    # 'var' means we use percent of explained variance
+    # 'mar-pa' means we use the marchenko-pastur distribution
+    if num_med == 'var' or num_med == 'mar-pa':
+      RRT_num_clusters = residual_returns_matrix[-clustering_window :, :]
+      cov = 1/(clustering_window) * (RRT_num_clusters.T @ RRT_num_clusters)
+      if num_med == 'var':
+        k = get_num_of_clusters(cov, 0.9)
+      if num_med == 'mar-pa':
+        rho = num_of_stocks / clustering_window
+        lambda_plus = (1 + np.sqrt(rho)) ** 2
+        print(lambda_plus)
+        eigs = eigh(cov, eigvals_only=True)
+        print(eigs)
+        plt.figure(figsize=(6, 4))
+        plt.hist(eigs, bins=20, edgecolor='black', alpha=0.7)
+        plt.title("Distribution of Eigenvalues")
+        plt.xlabel("Eigenvalue")
+        plt.ylabel("Frequency")
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
+        k = np.sum(eigs > lambda_plus)
+        print(k)
+    # 'self' means we pass in # of clusters ourselves
+    if num_med == 'self':
+      k = default_cluster_num
+    # split the correlation matrix into positive and negative parts
+    G_plus = np.maximum(corr, 0)
+    G_minus = np.maximum(-corr, 0)
+    # call the SPONGE algorithm
+    c = Cluster((csc_matrix(G_plus), csc_matrix(G_minus)))
+    predictions = c.SPONGE(k=k, tau_p=1, tau_n=1, eigens=None, mi=None)
+    # append predicted cluster assignments
+    R['cluster'] = predictions
+
+  # cluster stocks based on given industry data
+  # any stock that does not belong to the given list of stock-industry pairs
+  # is clustered into one single, separate cluster
+  if cl_med == 'industry':
+    ticker_to_cluster = dict(zip(sector['SPY.1'], sector['0']))
+    R['cluster'] = R['ticker'].map(ticker_to_cluster)
+    R['cluster'] = pd.to_numeric(R['cluster'], errors='coerce').astype('Int64')
+    max_cluster = R['cluster'].max()
+    R['cluster'] = R['cluster'].fillna(max_cluster + 1).astype(int)
+  return R
