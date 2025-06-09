2cb9a29ef407e489f84ddb4a262c403899077bba
Lunji Zhu <lunjizhu@Lunjis-MacBook-Pro.local>
2025-06-09 13:57:10 -0700

    set up inital experiment.py file

--- experiment.py
+++ experiment.py
@@ -32,6 +32,7 @@ parser.add_argument('--weight_type', choices=['uniform', 'linear', 'exponential'
 parser.add_argument('--winsorize_raw', action='store_true', help='Enable windsorization for the raw returns (default: False)')
 parser.add_argument('--winsorize_res', action='store_true', help='Enable windsorization for the res returns (default: False)')
 parser.add_argument('--winsor_param', type=float, default=0.05, help='Winsorization parameter (default: 0.05)')
+parser.add_argument('--num_dates', type=int, default=None, help='Number of eligible dates to use (default: None)')
 args = parser.parse_args()
 
 cluster_selection = args.cluster_selection
@@ -39,12 +40,13 @@ weight_type = args.weight_type
 winsorize_raw = args.winsorize_raw
 winsorize_res = args.winsorize_res
 winsor_param = args.winsor_param
+num_dates = args.num_dates
 
 print(f"cluster_selection: {cluster_selection}, weight_type: {weight_type}, windsorize_raw: {winsorize_raw}, windsorize_res: {winsorize_res}, winsor_param: {winsor_param}")
 
 eligible_dates_txt_output = path + '/eligible_dates.txt'
 eligible_dates = get_eligible_date_paths_from_file(eligible_dates_txt_output)
-print(f"Eligible dates loaded: {eligible_dates}")
+# print(f"Eligible dates loaded: {eligible_dates}")
 import time
 
 start_time = time.time()
@@ -54,16 +56,15 @@ daily_PnL, dates = execute_trading_strategy(win_threshold=0.1,
                                      w=5,
                                      eligible_dates=eligible_dates,
                                      cluster_selection=cluster_selection,
-                                     weighting_scheme=weight_type,
-                                     winsorize_raw=winsorize_raw,
-                                     winsorize_res=winsorize_res,
-                                     winsor_param=winsor_param)
+                                     num_dates=num_dates)
+                                    #  weighting_scheme=weight_type,
+                                    #  winsorize_raw=winsorize_raw,
+                                    #  winsorize_res=winsorize_res,
+                                    #  winsor_param=winsor_param)
 end_time = time.time()
 print(f"Execution time: {end_time - start_time:.2f} seconds")
 
 cumulative_pnl = np.cumsum(daily_PnL)
-
-import matplotlib.pyplot as plt
 
 # Save the plot in a 'results' folder in the same directory as this .py file
 try:
@@ -73,12 +74,14 @@ except NameError:
     script_dir = os.getcwd()
 results_dir = os.path.join(script_dir, 'results')
 os.makedirs(results_dir, exist_ok=True)
-plot_path = os.path.join(results_dir, 'cumulative_pnl.png')
-PnL_path = os.path.join(results_dir, 'daily_PnL.npy')
-date_path = os.path.join(results_dir, 'date.npy')
+run_setting = str(cluster_selection) + "_" + weight_type + "_" + str(winsorize_raw) + "_" + str(winsorize_res) + "_" + str(winsor_param)
 
-np.save(PnL_path, daily_PnL)
-np.save(date_path, dates)
+plot_path = os.path.join(results_dir, f'{run_setting}_cumulative_pnl.jpg')
+PnL_path = os.path.join(results_dir, f'{run_setting}_daily_PnL')
+date_path = os.path.join(results_dir, f'{run_setting}_date')
+
+np.savetxt(PnL_path + '.txt', daily_PnL)
+np.savetxt(date_path + '.txt', dates, fmt='%s')
 
 plt.figure(figsize=(14, 7))
 plt.plot(dates, cumulative_pnl)
